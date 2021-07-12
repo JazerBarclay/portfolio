939ae7a7aa31ee30d7236cb16bd0a7248d2d1201
Jazer <contact@jazer.co.uk>
2021-07-12 01:38:03 +0100

    Created light and dark theme switcher

---
+++ www/assets/scripts/theme.js
@@ -0,0 +1,17 @@
+const darkTheme = 'dark';
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+localStorage.setItem('selected-theme', getCurrentTheme());
+console.log(`Current theme: ${selectedTheme}`);
+
+if (selectedTheme) 
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+
+const themeButton = document.querySelector('#theme-button');
+
+themeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.body.classList.toggle(darkTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+})
