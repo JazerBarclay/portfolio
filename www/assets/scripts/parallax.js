d1835f80a7386940dc43dcf1dce33f33ba51af7b
Jazer <contact@jazer.co.uk>
2021-07-16 04:12:17 +0100

    Updated parallax speed

--- www/assets/scripts/parallax.js
+++ www/assets/scripts/parallax.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', function(e) {
             x.offsetTop + window.innerHeight/2 > window.scrollY && 
             x.offsetTop - window.innerHeight < (x.offsetHeight + window.scrollY)
           ) {
-            let offsetEl = (x.offsetTop - window.scrollY)/6 - ((x.offsetTop - window.scrollY)/12);
+            let offsetEl = ((x.offsetTop - window.scrollY)/6 - ((x.offsetTop - window.scrollY)/4)) * -1;
             x.style.transform = `scale(1.2) translateY(${offsetEl}px)`;
           }
     });
