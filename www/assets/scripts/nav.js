7663e011295ae568dab5eb3bac44e98238008d2a
Jazer <contact@jazer.co.uk>
2021-07-17 01:37:31 +0100

    Added active effect based on section visible

--- www/assets/scripts/nav.js
+++ www/assets/scripts/nav.js
@@ -19,4 +19,30 @@ window.addEventListener('scroll', function(e) {
   }
 
   new_scroll_position = last_scroll_position;
+
+  checkActive(
+    this.document.querySelector('#hero'), 
+    this.document.querySelector('#nav-home'));
+
+  checkActive(
+    this.document.querySelector('#projects'), 
+    this.document.querySelector('#nav-projects'));
+
+  checkActive(
+    this.document.querySelector('#about'), 
+    this.document.querySelector('#nav-about'));
+
+  checkActive(
+    this.document.querySelector('#contact'), 
+    this.document.querySelector('#nav-contact'));
+
+
 });
+
+function checkActive(section, nav) {
+  if (window.scrollY >= section.offsetTop - 1 && this.window.scrollY < section.offsetTop + section.offsetHeight - 1) {
+    nav.classList.add('active')
+  } else {
+    nav.classList.remove('active')
+  }
+}
