396a290026f1039dd9fd181e78df6105f2632a36
Jazer <contact@jazer.co.uk>
2021-07-27 16:01:41 +0100

    Fixed parallax speed, start and end points

--- www/assets/scripts/parallax.js
+++ www/assets/scripts/parallax.js
@@ -9,8 +9,8 @@ window.addEventListener('scroll', function(e) {
             let offsetEl = ((x.offsetTop - window.scrollY)/6 - ((x.offsetTop - window.scrollY)/4)) * -1;
             x.style.transform = `scale(1.2) translateY(${offsetEl}px)`;
             // console.log(offsetEl)
-          } else {
-            x.style.transform = `scale(1.2) translateY(40vh)`;
+          // } else {
+          //   x.style.transform = `scale(1.2) translateY(40vh)`;
           }
     });
 });
