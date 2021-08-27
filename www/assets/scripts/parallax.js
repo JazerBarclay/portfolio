96356f52299a0002696734d5f74d2a008ee24874
Jazer <contact@jazer.co.uk>
2021-08-27 02:01:50 +0100

    Reworked parallax to use GPU request animation and per element inertial scrolling

--- www/assets/scripts/parallax.js
+++ www/assets/scripts/parallax.js
@@ -1,14 +1,45 @@
 const elements = document.querySelectorAll('.parallax');
 
-window.addEventListener('scroll', function(e) {
-    elements.forEach(x => {
-        if (
-            x.offsetTop + window.innerHeight*1.5 > window.scrollY && 
-            x.offsetTop - window.innerHeight/2 < (x.offsetHeight + window.scrollY)
-          ) {
-            let offsetEl = ((x.offsetTop - window.scrollY)/6 - ((x.offsetTop - window.scrollY)/4)) * -1;
-            offsetEl = Math.round(offsetEl);
-            x.style.transform = `scale(1.2) translateY(${offsetEl}px)`;
-          }
-    });
+let currentY = [0];
+let aimY = [0];
+let speed = 0.2;
+let i = 0;
+
+elements.forEach(x => {
+  currentY[i] = 0;
+  aimY[i] = 0;
+  i++;
+})
+
+const animate = () => {
+  
+  i = 0;
+  elements.forEach(x => {
+
+    if (
+        x.offsetTop + window.innerHeight*1.5 > window.scrollY && 
+        x.offsetTop - window.innerHeight/2 < (x.offsetHeight + window.scrollY)
+    ) {
+      currentY[i] += (aimY[i] - currentY[i]) * speed;
+      x.style.transform = `scale(1.2) translateY(${currentY[i]}px)`;
+    }
+
+    i++;
+  })
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener("scroll", (e) => {
+
+  i = 0;
+  elements.forEach(x => {
+    let offsetEl = ((x.offsetTop - window.scrollY)/6 - ((x.offsetTop - window.scrollY)/4)) * -1;
+    offsetEl = Math.round(offsetEl);
+    aimY[i] = offsetEl;
+    i++;
+  })
+
 });
