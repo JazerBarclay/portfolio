1567ccc5d6d7b40245589ff46ae219a8e344820f
Jazer <contact@jazer.co.uk>
2021-08-27 10:58:17 +0100

    Added php form email handler

---
+++ www/mail.php
@@ -0,0 +1,37 @@
+<?php
+    // data sent in header are in JSON format
+    header('Content-Type: application/json');
+
+    // We are receiving JSON data too so lets decode it
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    // Takes the value from variables ready for sending
+    $name = $_POST['form_name'];
+    $email = $_POST['form_email'];
+    $postmessage = $_POST['form_message'];
+    $to = "contact@jazer.co.uk";
+    $subject = "Website inquiry from " . $name;
+
+    // Email template
+    $message = "<b>Name : </b>". $name . "<br>";
+    $message .= "<b>Email Address : </b>" . $email . "<br>";
+    $message .= "<b>Message : </b>" . $postmessage . "<br>";
+
+    $header = "From:".$email." \r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html\r\n";
+    $retval = mail ($to,$subject,$message,$header);
+
+    // Message Notification
+    if( $retval == true ) {
+        echo json_encode(array(
+            'success'=> true,
+            'message' => 'Message sent successfully'
+        ));
+    }else {
+        echo json_encode(array(
+            'error'=> true,
+            'message' => 'Error sending message'
+        ));
+    }
+?>
